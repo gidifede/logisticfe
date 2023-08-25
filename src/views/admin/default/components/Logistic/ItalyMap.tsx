f6452e45acc463022b5fe84d2360285de3e0fae3
difeder5 <difeder5@posteitaliane.it>
2023-08-25 15:02:50 +0200

    Working version no tooltip

--- src/views/admin/default/components/Logistic/ItalyMap.tsx
+++ src/views/admin/default/components/Logistic/ItalyMap.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import ReactTooltip from "react-tooltip";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4geodata_italyLow from "@amcharts/amcharts4-geodata/italyLow";
@@ -35,40 +36,6 @@ const ItalyMap = () => {
     imageSeriesTemplate.propertyFields.latitude = "latitude";
     imageSeriesTemplate.propertyFields.longitude = "longitude";
 
-    const tooltipDiv = document.createElement("div");
-    tooltipDiv.className = "custom-tooltip";
-    document.body.appendChild(tooltipDiv);
-
-    imageSeriesTemplate.events.on("over", (event) => {
-      const marker = event.target;
-      const cityData = marker.dataItem.dataContext;
-      const tooltipContent = `
-        <div class="bg-white text-black p-4 rounded shadow-lg">
-          <h1 class="text-xl font-bold">${cityData}</h1>
-          <p>Questo è il resto del testo</p>
-          <a href="https://it.wikipedia.org/wiki/${cityData}" target="_blank" class="text-blue-500">Maggiori informazioni</a>
-          <img src="https://www.example.com/${cityData}.jpg" alt="${cityData}" class="w-16 h-16 mt-2">
-          <button class="bg-blue-500 text-white p-2 mt-2 rounded" onclick="window.location.href='https://www.example.com/${cityData}'">Vai alla pagina</button>
-        </div>
-      `;
-
-      tooltipDiv.innerHTML = tooltipContent;
-
-      // Posizionamento orizzontale
-      const tooltipWidth = tooltipDiv.offsetWidth;
-      const x = marker.pixelX - tooltipWidth / 2;
-      tooltipDiv.style.left = `${x}px`;
-
-      // Posizionamento verticale
-      const y = marker.pixelY - tooltipDiv.offsetHeight - 10;
-      tooltipDiv.style.top = `${y}px`;
-
-      tooltipDiv.style.display = "block";
-    });
-
-    imageSeriesTemplate.events.on("out", () => {
-      tooltipDiv.style.display = "none";
-    });
     imageSeries.data = cityData;
 
     return map;
