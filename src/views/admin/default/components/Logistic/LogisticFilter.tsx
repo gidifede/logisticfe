d2e1bfb6522a964b6ccce70399e8cb8443eeac71
SCARPIELLO ANDREA (DTO) <SCARPIE3@posteitaliane.it>
2023-08-30 14:32:58 +0200

    added onclick for location and changed clicked theme

--- src/views/admin/default/components/Logistic/LogisticFilter.tsx
+++ src/views/admin/default/components/Logistic/LogisticFilter.tsx
@@ -1,23 +1,26 @@
 import Card from "components/card";
 
-const LogisticFilter = (props: { icon: JSX.Element; clicked: boolean }) => {
-  const { icon, clicked } = props;
+type LogisticFilterProps = {
+  icon: JSX.Element
+  clicked: boolean
+  onClick: () => void
+}
+
+
+const LogisticFilter = ({ icon, clicked, onClick }: LogisticFilterProps) => {
   return (
-    <>
       <div className="flex h-[60px] w-auto items-center">
         <div
           className={`rounded-full p-3 text-brand-500 ${
             clicked
-              ? "bg-[#442ff2] hover:bg-lightPrimary hover:text-white dark:bg-navy-700"
+              ? "bg-[#442ff2] text-white"
               : "bg-lightPrimary hover:bg-[#442ff2] hover:text-white"
           }`}
+          onClick={onClick}
         >
-          {/* Il tuo contenuto */}
-
-          <span className="flex items-center ">{icon}</span>
+        <span className="flex items-center">{icon}</span>
         </div>
       </div>
-    </>
   );
 };
 
--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -10,12 +10,16 @@ import Card from "components/card";
 const Dashboard = () => {
   const [selectedCity, setSelectedCity] = useState(null);
 
-  const [LocationHovered, setLocationHovered] = useState(true);
+  const [locationFlagSelected, setLocationFlag] = useState(true);
   const [clicked, setClicked] = useState(false);
   const [placeFilterSelected, setPlaceFilterSelected] = useState(true);
 
   const handleChildEvent = (newMessage: string) => {
     setSelectedCity(newMessage);
+  };
+
+  const handleLocationFlagSelect = () => {
+    setLocationFlag(!locationFlagSelected);
   };
 
   return (
@@ -24,15 +28,18 @@ const Dashboard = () => {
         <div className="!z-5 relative flex flex-grow flex-col items-center rounded-[20px] rounded-[20px] bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none">
           <LogisticFilter
             icon={<MdPlace className={`h-7 w-7`} />}
-            clicked={false}
+            clicked={locationFlagSelected}
+            onClick={handleLocationFlagSelect}
           />
           <LogisticFilter
             icon={<MdDirections className={`h-7 w-7`} />}
             clicked={false}
+            onClick={() => {}}
           />
           <LogisticFilter
             icon={<MdDirectionsCar className={`h-7 w-7 `} />}
             clicked={false}
+            onClick={() => {}}
           />
         </div>
       </div>
