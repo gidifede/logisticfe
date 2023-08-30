9a5b150e47e99b45a80b0cd135fcf8c43f93dfac
difeder5 <difeder5@posteitaliane.it>
2023-08-30 12:00:28 +0200

    fixed over bug

--- src/views/admin/default/components/Logistic/LogisticFilter.tsx
+++ src/views/admin/default/components/Logistic/LogisticFilter.tsx
@@ -1,11 +1,19 @@
 import Card from "components/card";
 
-const LogisticFilter = (props: { icon: JSX.Element; title: string }) => {
-  const { icon, title } = props;
+const LogisticFilter = (props: { icon: JSX.Element; clicked: boolean }) => {
+  const { icon, clicked } = props;
   return (
     <>
-      <div className="ml-[18px] flex h-[60px] w-auto items-center">
-        <div className="rounded-full bg-lightPrimary p-3 text-brand-500 hover:bg-[#442ff2] hover:text-white dark:bg-navy-700  dark:text-white">
+      <div className="flex h-[60px] w-auto items-center">
+        <div
+          className={`rounded-full p-3 text-brand-500 ${
+            clicked
+              ? "bg-[#442ff2] hover:bg-lightPrimary hover:text-white dark:bg-navy-700"
+              : "bg-lightPrimary hover:bg-[#442ff2] hover:text-white"
+          }`}
+        >
+          {/* Il tuo contenuto */}
+
           <span className="flex items-center ">{icon}</span>
         </div>
       </div>
--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -10,7 +10,7 @@ import Card from "components/card";
 const Dashboard = () => {
   const [selectedCity, setSelectedCity] = useState(null);
 
-  const [LocationHovered, setLocationHovered] = useState(false);
+  const [LocationHovered, setLocationHovered] = useState(true);
   const [clicked, setClicked] = useState(false);
   const [placeFilterSelected, setPlaceFilterSelected] = useState(true);
 
@@ -21,28 +21,20 @@ const Dashboard = () => {
   return (
     <div className="mt-10 flex h-screen items-start">
       <div className="w-[120px]  p-4">
-        <Card extra="flex-grow items-center rounded-[20px]">
+        <div className="!z-5 relative flex flex-grow flex-col items-center rounded-[20px] rounded-[20px] bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none">
           <LogisticFilter
-            icon={
-              <MdPlace
-                className={`hover:bg-sky-700 h-7 w-7 ${
-                  (LocationHovered && !clicked) || (!LocationHovered && clicked)
-                    ? "text-red-500"
-                    : ""
-                }`}
-              />
-            }
-            title={"SDA Facilities"}
+            icon={<MdPlace className={`h-7 w-7`} />}
+            clicked={false}
           />
           <LogisticFilter
             icon={<MdDirections className={`h-7 w-7`} />}
-            title={"Routes"}
+            clicked={false}
           />
           <LogisticFilter
             icon={<MdDirectionsCar className={`h-7 w-7 `} />}
-            title={"Fleet"}
+            clicked={false}
           />
-        </Card>
+        </div>
       </div>
 
       <div className="flex-grow">
